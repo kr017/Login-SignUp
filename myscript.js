343650a9b52a0e72507ac5d132f044b2cc9c10d1
Kiran <krmate634@gmail.com>
2021-01-02 11:07:18 +0530

    need # for id

--- myscript.js
+++ myscript.js
@@ -1,9 +1,15 @@
- sliderLeftBtn= document.querySelector("btn-signin")
-const sliderRightBtn=document.querySelector("btn-signup");
-const leftPanel=document.querySelector("left-panel");
-const rightPanel=document.querySelector("right-panel");
+// window.addEventListener("load",function(){
 
 
-// sliderLeftBtn.addEventListener("click",()=>{
-//     console.log("rushabh")
-// })
+const sliderLeftBtn=document.querySelector("#btn-signin");
+// const sliderRightBtn=document.querySelector("btn-signup");
+// const leftPanel=document.querySelector("left-panel");
+// const rightPanel=document.querySelector("right-panel");
+
+// alert("Im Here");
+sliderLeftBtn.addEventListener("click",()=>{
+    alert("rushabh")
+})
+
+
+// });
